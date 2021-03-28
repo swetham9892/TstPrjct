ae338741d1012cb9da07ba6679b28a397583ef39
DELL <DELL@DESKTOP-P8N6FVG>
2021-03-28 22:59:32 +0530

    Frst cmt

--- Automatioon_Prac/src/test/java/Pgms/LngNmbrs.java
+++ Automatioon_Prac/src/test/java/Pgms/LngNmbrs.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	long lngNoWthoutL = 1000*60*60*24*365;
 	long lngNoWthL = 1000*60*60*24*365L;  
 	System.out.println(lngNoWthoutL);//1471228928 ----considered as int
+	System.out.println("L means Long");
 	System.out.println(lngNoWthL);//31536000000-----36 bit------considered as long long = 8bytes =36 bits
 	//11101010111101100010010110000000000 when decimal cnvrtd to binary
 	//bt int limi is 32 bit only then java truncates 1st 4 digits (36-34=4) to make it 32 bit ie
